93df740ae43dbb7b77c958d7fb50d4fbf8472bf2
youngsan01 <intokys@gmail.com>
2024-05-28 14:08:05 +0900

    Service

--- src/main/java/org/choongang/member/service/JoinService.java
+++ src/main/java/org/choongang/member/service/JoinService.java
@@ -1,6 +1,9 @@
 package org.choongang.member.service;
 
-public class JoinService implements Service<RequestJoin>{
+import org.choongang.global.Service;
+import org.choongang.member.controller.RequestJoin;
+
+public class JoinService implements Service<RequestJoin> {
 
     @Override
     public void process(RequestJoin form) {
