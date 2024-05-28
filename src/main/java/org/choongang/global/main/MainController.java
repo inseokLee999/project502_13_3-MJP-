6450c7264f33378461bf2e765a823169d0f5fb40
jangjangji <sjjang41@naver.com>
2024-05-28 11:10:27 +0900

    중간변경

---
+++ src/main/java/org/choongang/global/main/MainController.java
@@ -0,0 +1,10 @@
+package org.choongang.global.main;
+
+import org.choongang.global.AbstractController;
+
+public class MainController extends AbstractController {
+    @Override
+    public void show() {
+        System.out.println("메인메뉴");
+    }
+}
---
+++ src/main/java/org/choongang/global/member/LoginController.java
@@ -0,0 +1,10 @@
+package org.choongang.global.member;
+
+import org.choongang.global.AbstractController;
+
+public class LoginController extends AbstractController {
+    @Override
+    public void show() {
+        System.out.println("로그인 페이지");
+    }
+}
