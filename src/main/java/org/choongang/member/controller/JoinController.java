51a6ac22d08a4486352360547334adf1cd0dcdb8
jangjangji <sjjang41@naver.com>
2024-05-28 12:10:20 +0900

    기본설정완료

--- src/main/java/org/choongang/member/controller/JoinController.java
+++ src/main/java/org/choongang/member/controller/JoinController.java
@@ -1,10 +1,12 @@
 package org.choongang.member.controller;
 
 import org.choongang.global.AbstractController;
+import org.choongang.global.constants.Menu;
+import org.choongang.template.Templates;
 
 public class JoinController extends AbstractController {
     @Override
     public void show() {
-        System.out.println("회원가입페이지");
+        Templates.getInstance().render(Menu.JOIN);
     }
 }
--- src/main/java/org/choongang/template/Templates.java
+++ src/main/java/org/choongang/template/Templates.java
@@ -1,6 +1,6 @@
 package org.choongang.template;
 
-import org.choongang.constants.Menu;
+import org.choongang.global.constants.Menu;
 import org.choongang.template.game.GameTpl;
 import org.choongang.template.game.ResultTpl;
 import org.choongang.template.main.MainTpl;
@@ -9,38 +9,34 @@ import org.choongang.template.member.LoginTpl;
 
 import java.util.HashMap;
 import java.util.Map;
-public class Templates {
-    private static Templates instance;
-    private Map<Menu,Template> tpls;
-    private Templates(){
-        tpls = new HashMap<>();
-    }
 
-
+public  class Templates {
+    private static  Templates instance;
+    private Map<Menu, Template> tpls;
+    private Templates(){tpls = new HashMap<>();}
     public static Templates getInstance(){
-        if(instance==null){
+        if(instance == null){
             instance = new Templates();
         }
         return instance;
     }
-    public void render(Menu menu){
+    public void render(Menu menu) {
+
         System.out.println(find(menu).getTpl());
     }
     public Template find(Menu menu){
-        Template tpl = tpls.get(menu);
-        if(tpl != null){
-            return tpl;
-        }
+    Template tpl = tpls.get(menu);
+    if(tpl != null){
+        return tpl;
+    }
         switch (menu){
-            case JOIN: tpl = new JoinTpl();break;
-            case LOGIN : tpl = new LoginTpl();break;
+            case JOIN: tpl = new JoinTpl(); break;
+            case LOGIN: tpl = new LoginTpl(); break;
             case GAME: tpl = new GameTpl(); break;
-            case RESULT: tpl = new ResultTpl();break;
-            default : tpl = new MainTpl();
+            case RESULT: tpl = new ResultTpl(); break;
+            default: tpl = new MainTpl();
         }
         tpls.put(menu,tpl);
         return tpl;
     }
-    public String line(){return "----------------------------------------\n";}
-    public String doubleline(){return "========================================\n";}
 }
