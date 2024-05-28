90612479acfd8e792aae10f63e87a9d9d0c292c1
inseokLee999 <syt06137@naver.com>
2024-05-28 10:43:54 +0900

    Templates, GameTpl, LoginTpl

--- src/main/java/org/choongang/template/game/GameTpl.java
+++ src/main/java/org/choongang/template/game/GameTpl.java
@@ -3,4 +3,8 @@ package org.choongang.template.game;
 import org.choongang.template.Template;
 
 public class GameTpl implements Template {
+    @Override
+    public String getTpl() {
+        return "";
+    }
 }
--- src/main/java/org/choongang/template/member/LoginTpl.java
+++ src/main/java/org/choongang/template/member/LoginTpl.java
@@ -1,6 +1,4 @@
 package org.choongang.template.member;
 
-import org.choongang.template.Template;
-
-public class LoginTpl implements Template {
+public class LoginTpl {
 }
